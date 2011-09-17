5a4c6aba33deafa2ae1444ecae6717225b49f44d
Doug Hodson <doug@openeaagles.org>
2011-09-17 11:44:54 -0400

    fixed documentation

--- include/openeaagles/basic/StateMachine.h
+++ include/openeaagles/basic/StateMachine.h
@@ -195,7 +195,7 @@ namespace Basic {
 //
 //
 // Examples of state machines can be found in the OpenEaagles
-// example applicaiton 'mainStMach'.
+// example applicaiton 'testStateMach'.
 //------------------------------------------------------------------------------
 class StateMachine : public Component {
    DECLARE_SUBCLASS(StateMachine,Component)
