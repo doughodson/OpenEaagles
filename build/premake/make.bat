40642ac373f3b0dff5154a1263239f5d990116d2
Doug Hodson <doug@openeaagles.org>
2013-03-09 15:39:52 -0500

    updated premake make file

--- build/premake/make.bat
+++ build/premake/make.bat
@@ -6,8 +6,8 @@ rem @SET Premake=..\..\..\OpenEaagles3rdParty\bin\premake_v4.4.dev.exe
 rem
 rem delete old solution/workspace files
 rem
-rem rmdir /S /Q ..\codeblocks
-rem rmdir /S /Q ..\codelite
+rmdir /S /Q ..\codeblocks
+rmdir /S /Q ..\codelite
 rmdir /S /Q ..\vs2008
 rmdir /S /Q ..\vs2010
 rmdir /S /Q ..\vs2012
@@ -19,10 +19,10 @@ rmdir /S /Q ..\..\lib\vs2008
 rmdir /S /Q ..\..\lib\vs2010
 rmdir /S /Q ..\..\lib\vs2012
 rem
-rem create solution/workspace files
+rem create new solution/workspace files
 rem
-rem %Premake% codeblocks
-rem %Premake% codelite
+%Premake% codeblocks
+%Premake% codelite
 %Premake% vs2008
 %Premake% vs2010
 %Premake% vs2010 vs2012
