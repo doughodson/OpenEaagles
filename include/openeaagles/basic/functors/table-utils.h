8442f5f3ec0ddc4a07ff148c5645fa350bb6d619
Doug Hodson <doug@openeaagles.org>
2015-04-12 22:14:09 -0400

    separated linear function interpolators from Table class

---
+++ include/openeaagles/basic/functors/table-utils.h
@@ -0,0 +1,116 @@
+
+#ifndef __Eaagles_Basic_table_utils_H__
+#define __Eaagles_Basic_table_utils_H__
+
+#include "openeaagles/basic/support.h"
+
+namespace Eaagles {
+namespace Basic {
+
+// ---
+// 1D Linear Function Interpolator
+//    x       - Independent variable #1
+//    x_data  - Table of independent variable #1 breakpoints
+//    nx      - Size of x_data table
+//    xbp     - Pointer to the previous X breakpoint (optional)
+//    a_data  - Table of dependent variable data
+//    eFlg    - Extrapolation enabled flag
+// ---
+
+LCreal lfi(const LCreal x,
+           const LCreal *x_data, const unsigned int nx,
+           const LCreal *a_data,
+           const bool eFlg,
+           unsigned int* const xbp=0
+          );
+
+// ---
+// 2D Linear Function Interpolator
+//    Same as 1D plus ...
+//       y       - Independent variable #2
+//       y_data  - Table of independent variable #2 breakpoints
+//       ny      - Size of y_data table
+//       ybp     - Pointer to the previous Y breakpoint (optional)
+// ---
+
+LCreal lfi(const LCreal x, const LCreal y,
+           const LCreal *x_data, const unsigned int nx,
+           const LCreal *y_data, const unsigned int ny,
+           const LCreal *a_data,
+           const bool eFlg,
+           unsigned int* const xbp=0,
+           unsigned int* const ybp=0
+          );
+
+// ---
+// 3D Linear Function Interpolator
+//    Same as 2D plus ...
+//       z       - Independent variable #3
+//       z_data  - Table of independent variable #3 breakpoints
+//       nz      - Size of z_data table
+//       zbp     - Pointer to the previous Z breakpoint (optional)
+// ---
+
+LCreal lfi(const LCreal x, const LCreal y, const LCreal z,
+           const LCreal *x_data, const unsigned int nx,
+           const LCreal *y_data, const unsigned int ny,
+           const LCreal *z_data, const unsigned int nz,
+           const LCreal *a_data,
+           const bool eFlg,
+           unsigned int* const xbp=0,
+           unsigned int* const ybp=0,
+           unsigned int* const zbp=0
+          );
+
+// ---
+// 4D Linear Function Interpolator
+//    Same as 3D plus ...
+//       w       - Independent variable #4
+//       w_data  - Table of independent variable #4 breakpoints
+//       nw      - Size of w_data table
+//       wbp     - Pointer to the previous W breakpoint (optional)
+// ---
+
+LCreal lfi(const LCreal x, const LCreal y, const LCreal z, const LCreal w,
+           const LCreal *x_data, const unsigned int nx,
+           const LCreal *y_data, const unsigned int ny,
+           const LCreal *z_data, const unsigned int nz,
+           const LCreal *w_data, const unsigned int nw,
+           const LCreal *a_data,
+           const bool eFlg,
+           unsigned int* const xbp=0,
+           unsigned int* const ybp=0,
+           unsigned int* const zbp=0,
+           unsigned int* const wbp=0
+          );
+
+// ---
+// 5D Linear Function Interpolator
+//    Same as 4D plus ...
+//       v       - Independent variable #5
+//       v_data  - Table of independent variable #5 breakpoints
+//       nv      - Size of v_data table
+//       vbp     - Pointer to the previous V breakpoint (optional)
+// ---
+
+LCreal lfi(const LCreal x, const LCreal y, const LCreal z, const LCreal w, const LCreal v,
+         const LCreal *x_data, const unsigned int nx,
+         const LCreal *y_data, const unsigned int ny,
+         const LCreal *z_data, const unsigned int nz,
+         const LCreal *w_data, const unsigned int nw,
+         const LCreal *v_data, const unsigned int nv,
+         const LCreal *a_data,
+         const bool eFlg,
+         unsigned int* const xbp=0,
+         unsigned int* const ybp=0,
+         unsigned int* const zbp=0,
+         unsigned int* const wbp=0,
+         unsigned int* const vbp=0
+      );
+
+
+}
+}
+
+#endif
+
