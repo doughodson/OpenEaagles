206f5945bc876c64d33613db3bfa3e10bdd6bc65
Doug Hodson <doug@openeaagles.org>
2016-07-05 21:10:55 -0400

    updated parser code with better error msgs and corrected line number counting in scanner

--- src/base/EdlParser.hpp
+++ src/base/EdlParser.hpp
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.0.2.  */
+/* A Bison parser, made by GNU Bison 3.0.4.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -56,10 +56,10 @@ extern int yydebug;
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE YYSTYPE;
+
 union YYSTYPE
 {
-#line 108 "edl_parser.y" /* yacc.c:1909  */
+#line 113 "edl_parser.y" /* yacc.c:1909  */
 
    double                     dval;
    long                       lval;
@@ -73,6 +73,8 @@ union YYSTYPE
 
 #line 75 "EdlParser.hpp" /* yacc.c:1909  */
 };
+
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define YYSTYPE_IS_DECLARED 1
 #endif
