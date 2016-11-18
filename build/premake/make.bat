dbbfb39b5e25dc4dd1816c7883d3d26ffb6574f9
Doug Hodson <doug@openeaagles.org>
2016-11-18 13:14:48 -0500

    updated premake batch file to use alpha 10

--- build/premake/make.bat
+++ build/premake/make.bat
@@ -1,7 +1,7 @@
 rem
 rem set premake location
 rem
-@SET Premake=..\..\..\OpenEaagles3rdParty\bin\premake5-alpha9.exe
+@SET Premake=..\..\..\OpenEaagles3rdParty\bin\premake5-alpha10.exe
 rem
 rem delete old solution/workspace files
 rem
